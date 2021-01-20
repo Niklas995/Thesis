c980325505e392e584df62c9155b033e65297712
Niklas995 <77723422+Niklas995@users.noreply.github.com>
2021-01-20 22:18:57 +0100

    Update ev_wi.jl

--- ev_wi.jl
+++ ev_wi.jl
@@ -1,26 +1,4 @@
 
-import Pkg
-Pkg.add("OffsetArrays")
-Pkg.add("Plots")
-Pkg.add("ArgParse")
-
-
-#!/usr/local/bin/julia
-
-# arrays with arbitrary starting index
-using OffsetArrays
-# statistics package
-using Statistics
-# plotting
-using Plots
-# delimited fields file output
-using DelimitedFiles
-# command line argument parsing
-using ArgParse
-# LAPACK
-using LinearAlgebra
-
-
 
 
 β= 3.2
